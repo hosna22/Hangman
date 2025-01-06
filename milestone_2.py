f1a2d89b3570ee4c9ad3fe325d71612068e7f306
Hosna Shams <hosna.s123@gmail.com>
2025-01-06 20:04:01 +0000

    Creates milestone_2.py file. Imports random to test random.choice method, and uses if else statement to check if user input is a single letter.

---
+++ milestone_2.py
@@ -0,0 +1,14 @@
+import random
+
+guess = input("Enter a single letter: ")
+
+if len(guess) == 1 and guess.isalpha() == True:
+    print("Good guess!")
+else: 
+    print("Oops! That is not a valid input.")
+
+word_list = ["Mango", "Blueberries", "Watermelon", "Kiwi", "Apples"]
+
+random_word = random.choice(word_list)
+
+print(random_word)
