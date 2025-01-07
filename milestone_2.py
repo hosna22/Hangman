57c5896abf6b0f78a7087b3c184f95c782ec7331
Hosna Shams <hosna.s123@gmail.com>
2025-01-07 11:19:19 +0000

    Put user input into a function in file milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,14 +1,17 @@
 import random
 
-guess = input("Enter a single letter: ")
-
-if len(guess) == 1 and guess.isalpha() == True:
-    print("Good guess!")
-else: 
-    print("Oops! That is not a valid input.")
-
 word_list = ["Mango", "Blueberries", "Watermelon", "Kiwi", "Apples"]
 
 random_word = random.choice(word_list)
 
 print(random_word)
+
+guess = input("Enter a single letter: ")
+
+def check_input():
+    if len(guess) == 1 and guess.isalpha() == True:
+        print("Good guess!")
+    else: 
+        print("Oops! That is not a valid input.")
+
+check_input()
