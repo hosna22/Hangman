0ecb53a29201e22fc6d22d2c4fbb68d2c5aeb677
Hosna Shams <hosna.s123@gmail.com>
2025-01-08 11:13:15 +0000

    Adds docstrings to milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -1,16 +1,31 @@
 import random
 
 class Hangman:
-
+    """
+    Attributes:
+        word_list (list): list of words for player to guess
+        num_lives (int): number of lives the player has at the start of the game
+        word (str): word to be guessed
+        word_guessed (list): list of empty spaces for the letters of the word
+        num_letter (int): number of unique letters in the word that have not been guessed yet
+        list_of_guesses (list): list of the guesses that have already been tried
+        """
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
-        self.num_lives = num_lives  #number of lives the player has at the start of the game
-        self.word = random.choice(word_list)  #word to be guessed
-        self.word_guessed = ['_' for letter in self.word] #list of the letters of the word
-        self.num_letters = len(self.word)  #number of UNIQUE letters in the word that have not been guessed yet
-        self.list_of_guesses = []  #A list of the guesses that have already been tried
+        self.num_lives = num_lives 
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for letter in self.word] 
+        self.num_letters = len(self.word)  
+        self.list_of_guesses = [] 
     
     def check_guess(self,guess):
+        """
+        This method checks if the the user input guess is in the word to be guessed. 
+        
+        The method passes the guess from the user as a parameter. It checks if the guess 
+        is in the random word to be guessed and returns the letters position in the word 
+        if guessed correctly. If guessed incorrectly, it reduces the players lives by 1. 
+        """
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
@@ -25,6 +40,14 @@ class Hangman:
             print(f"you have {self.num_lives} lives left")
 
     def ask_for_input(self):
+        """
+        This method checks if the the user input is a single letter and acceptable input. 
+        
+        The method asks the player to input their guess and checks if it is one character 
+        and alphabetical. If the input is not correct, the error is stated and the user is 
+        asked to input again. If the input passes the checks, it is appended to a list which 
+        stores the players guesses to avoid the same letter being guessed twice.
+        """
         print(self.word_guessed)
         while True: 
             guess = input("Enter a single letter: ").lower()
